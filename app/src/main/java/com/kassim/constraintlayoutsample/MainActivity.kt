ffc9f163697be9126d2bcc4e0982ade7a8d2e892
shehryarahmed <shehreyarahmed5@gmail.com>
2019-11-01 11:01:49 +0500

    add place holder it amazing feature in constraint layout

--- app/src/main/java/com/kassim/constraintlayoutsample/MainActivity.kt
+++ app/src/main/java/com/kassim/constraintlayoutsample/MainActivity.kt
@@ -2,12 +2,22 @@ package com.kassim.constraintlayoutsample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.transition.TransitionManager
+import android.view.View
+import androidx.constraintlayout.widget.Placeholder
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
     }
 
+    fun swapView(view : View){
+        TransitionManager.beginDelayedTransition(main_layout)
+        place_holder.setContentId(view.id)
+    }
 }
